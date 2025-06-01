d07355f4ac31d6ee74ba7f9accfe574e393c1d08
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2025-06-01 11:45:27 +0200

    init commit

---
+++ src/main/java/com/kwizera/springbootamalitechlab09taskmanager/SpringbootAmalitechLab09TaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.kwizera.springbootamalitechlab09taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAmalitechLab09TaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAmalitechLab09TaskManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kwizera/springbootamalitechlab09taskmanager/SpringbootAmalitechLab09TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwizera.springbootamalitechlab09taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAmalitechLab09TaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
